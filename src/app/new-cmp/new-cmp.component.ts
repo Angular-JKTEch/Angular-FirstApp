9312f175cc634b99ee7a3f41549e8a074483cbf5
ngactive99 <ngactive99@gmail.com>
2022-03-01 15:16:43 +0530

    ng class and ng style

--- src/app/new-cmp/new-cmp.component.ts
+++ src/app/new-cmp/new-cmp.component.ts
@@ -6,18 +6,42 @@ import { Component, Input, OnInit,Output, EventEmitter } from '@angular/core';
   styleUrls: ['./new-cmp.component.css'] 
 })
 export class NewCmpComponent implements OnInit {
+  
+  canSave = true;
+  isUnchanged = true;
+  isSpecial = true;
+  currentClasses: Record<string, boolean> = {};
+  currentStyle: Record<string, string> = {};
+  ngOnInit() {
+    this.setCurrentClasses() ;
+    this.setCurrentStyle();
+  }
+  setCurrentClasses() {
+    // css classes : added or removed per the state of our compoennt properties 
+    this.currentClasses = {
+      saveable: this.canSave,
+      modified: !this.isUnchanged,
+      special: this.isSpecial
+    }
+  }
+  setCurrentStyle() {
+    this.currentStyle = {
+      'font-style': this.canSave ? 'italic' : 'normal',
+      'font-weight': this.isUnchanged ? 'bold' : 'normal',
+      'font-size': this.isSpecial ? '24px' : "12px",
+    }
+  }
 
-  @Input() size = " "; // decorate the property with input 
+  // @Input() size = " "; // decorate the property with input 
 
-  @Output() newItemEvent = new EventEmitter<string>();
+  // @Output() newItemEvent = new EventEmitter<string>();
 
-  addNewItem(value:string) {
-    this.newItemEvent.emit(value);
-  } // all items will be in string 
+  // addNewItem(value:string) {
+  //   this.newItemEvent.emit(value);
+  // } // all items will be in string 
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  
 
 }
