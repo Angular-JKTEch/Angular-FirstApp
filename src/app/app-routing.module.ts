3e6e79e5aa8a25e682abdfa3220110a692c5467f
ngactive99 <ngactive99@gmail.com>
2022-03-03 12:33:44 +0530

    created app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,26 @@ import { ContractFormComponent } from "./contract-form/contract-form.component";
 import { NewCmpComponent } from "./new-cmp/new-cmp.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
 
-const routes : Routes = [
-    {
-      path: 'contract-form',
-      component: ContractFormComponent
-    },
-    { 
-      path: 'new-cmp',
-      component: NewCmpComponent
-    },
-    { 
-      path: ' **',
-      pathMatch: 'full',
-      component: NotFoundComponent
-    },
-
-  ]
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports : [RouterModule],
-  })
-
-
-  export class AppRoutingModule{};
+const routes: Routes = [
+ 
+  {
+    path: "contract-form",
+    component: ContractFormComponent,
+  },
+  {
+    path: "new-cmp",
+    component: NewCmpComponent,
+  },
+  {
+    path: " **",
+    pathMatch: "full",
+    component: NotFoundComponent,
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
